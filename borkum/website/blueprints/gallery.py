c52e837cf2408ed68017774916f140339708f4ce
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2022-03-22 15:58:24 +0100

    Updates gallery

--- borkum/website/blueprints/gallery.py
+++ borkum/website/blueprints/gallery.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import random
 from flask import Blueprint, render_template
 import os
 
@@ -8,9 +10,16 @@ def init():
     images = []
     for filename in os.listdir('borkum/website/static/img/Gallery'):
         if filename.split(".")[-1] in ["png", "jpg", "jpeg"]:
-            print(filename)
             images.append(os.path.join('\\static\\img\\Gallery', filename))
         else:
             continue
-    return render_template("gallery.html", images= images)
+
+    thumbnails = []
+    for filename in os.listdir('borkum/website/static/img/Gallery/Thumbnail'):
+        if filename.split(".")[-1] in ["png", "jpg", "jpeg"]:
+            thumbnails.append(('/static/img/Gallery/Thumbnail/' + filename))
+        else:
+            continue
+
+    return render_template("gallery.html", images= images, thumbnail= thumbnails[random.randint(0, len(thumbnails)-1)])
 
