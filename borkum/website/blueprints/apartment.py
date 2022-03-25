5827a2421204973a475c6d92fe3e6f374e22a407
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2022-03-25 10:17:58 +0100

    Hopefully finally a working version

--- borkum/website/blueprints/apartment.py
+++ borkum/website/blueprints/apartment.py
@@ -9,16 +9,16 @@ def init(fewo):
     import os
     res_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
 
-    data = json.load(open(os.path.join(res_path, 'content\\apartments.json'), encoding='utf-8'))
+    data = json.load(open(os.path.join(res_path, 'content', 'apartments.json'), encoding='utf-8'))
     images = []
 
-    for filename in os.listdir(os.path.join(res_path, 'img\\apartments\\' + fewo + '\\rooms')):
+    for filename in os.listdir(os.path.join(res_path, 'img', 'apartments', fewo ,'rooms')):
         if filename.split(".")[-1].lower() in ["png", "jpg", "jpeg"]:
-            images.append(os.path.join('\\static\\img\\apartments\\' + fewo + '\\rooms', filename))
+            images.append(os.path.join('/', 'static', 'img', 'apartments',  fewo, 'rooms', filename))
         else:
             continue
-    filename = os.listdir(os.path.join(res_path, 'img\\apartments\\' + fewo + '\\thumbnail'))[0]
-    thumbnail = '\\static\\img\\apartments\\' + fewo + '\\thumbnail\\' + filename
+    filename = os.listdir(os.path.join(res_path, 'img', 'apartments', fewo, 'thumbnail'))[0]
+    thumbnail = os.path.join('/', 'static', 'img', 'apartments',  fewo, 'thumbnail', filename)
     
     return render_template("apartment.html", info=data[fewo], images=images, thumbnail=thumbnail)
 
