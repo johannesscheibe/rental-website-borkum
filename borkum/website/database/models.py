75a1c4072cd5087a46b2fb211f7c23f919c0a9c9
Johannes <scheibe.johannes@t-online.de>
2022-11-18 19:08:50 +0100

    New db models

--- borkum/website/database/models.py
+++ borkum/website/database/models.py
@@ -1,12 +1,17 @@
-import json
-
-from flask_sqlalchemy import SQLAlchemy
-from . import db
-
-import enum
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import relationship
 from flask import current_app as app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import (
+    Boolean,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Table,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import relationship
+
+from . import db
 
 db: SQLAlchemy
 
@@ -21,66 +26,95 @@ class Base():
         return d
 
 
-image_mapping = db.Table(
-    "Image Mapping",
-    db.Column("object_id", db.ForeignKey("RentalObject.id")),
-    db.Column("image_id", db.ForeignKey("Image.id")),
+class TagCategory(db.Model, Base):
+    __tablename__ = "tag_category"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+    __table_args__ = (UniqueConstraint("name"),)
+
+class Tag(db.Model, Base):
+    __tablename__ = "tag"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    icon = Column(String(20))
+    category_id = Column(Integer, ForeignKey("tag_category.id"))
+
+    __table_args__ = (UniqueConstraint("name"),)
+
+class Image(db.Model, Base):
+    __tablename__ = "image"
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String(50))
+    title = Column(String(20))
+    description = Column(String(100))
+
+    #__table_args__ = (UniqueConstraint("filename"),)
+
+
+class House(db.Model, Base):
+    __tablename__ = "house"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    description = Column(String())
+
+    address = Column(String(15))
+    visible = Column(Boolean())
+
+    # One to Many House <-> Image
+    images = relationship("HouseImages", back_populates="house")
+
+    # One to Many House <-> Apartment
+    apartments = relationship("Apartment", back_populates="house")
+
+
+class Apartment(db.Model, Base):
+    __tablename__ = "apartment"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    description = Column(String())
+
+    # Assosiation Apartment <-> Image
+    images = relationship("ApartmentImages", back_populates="apartment")
+
+    # Many to One Apartment <-> House
+    house_id = Column(Integer, ForeignKey("house.id"))
+    house = relationship("House", back_populates="apartments")
+
+    # Many to Many Apartment <-> Tag
+    tags = relationship("Tag", secondary='association_table', backref="apartments")
+
+
+class ApartmentImages(db.Model, Base):
+    __tablename__ = "apartment_images"
+    apartment_id = Column(ForeignKey("apartment.id"), primary_key=True)
+    image_id = Column(ForeignKey("image.id"), primary_key=True)
+    
+    is_thumbnail = Column(Boolean())
+
+    apartment = relationship("Apartment", back_populates="images")
+    image = relationship("Image")
+
+
+class HouseImages(db.Model, Base):
+    __tablename__ = "house_images"
+    apartment_id = Column(ForeignKey("house.id"), primary_key=True)
+    image_id = Column(ForeignKey("image.id"), primary_key=True)
+    
+    is_thumbnail = Column(Boolean())
+
+    house = relationship("House", back_populates="images")
+    image = relationship("Image")
+
+
+association_table = Table(
+    "association_table",
+    db.Model.metadata,
+    Column("apartment_id", ForeignKey("apartment.id")),
+    Column("tag_id", ForeignKey("tag.id")),
 )
-
-tag_mapping = db.Table(
-    "Tag Mapping",
-    db.Column("flat_id", db.ForeignKey("Apartment.id")),
-    db.Column("tag_id", db.ForeignKey("Tag.id")),
-)
-
-class RentalObject(Base):
-    __tablename__ = "RentalObject"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    description = db.Column(db.String())
-
-    thumbnail_id = db.Column(db.Integer, db.ForeignKey("Image.id"))
-    thumbnail = relationship("Image") # Many to One Relationship
-    
-    images = relationship("Image", secondary=image_mapping) # Many to Many Relationship
-
-    __mapper_args__ = {'polymorphic_on': building_type}
-
-class House(RentalObject, db.Model):
-    __mapper_args__ = {'polymorphic_identity': 'house'}
-    
-    address = db.Column(db.String(15))
-    visible = db.Column(db.Boolean())
-
-    __table_args__ = (UniqueConstraint('name'),)
-
-class Apartment(RentalObject, db.Model):
-    __mapper_args__ = {'polymorphic_identity': 'apartment'}
-
-
-
-    house_id = db.Column(db.Integer, db.ForeignKey('House.id'))
-    house = relationship('House')  # Many to One Relationship
-
-    tags = relationship("Tag", secondary=tag_mapping) # Many to Many Relationship
-    
-    __table_args__ = (UniqueConstraint('name'),)
-
-class Tag(Base, db.Model):
-    __tablename__ = "Tag"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    icon = db.Column(db.String(20))
-    # category = db.Column(db.String(20))
-
-class Image(Base, db.Model):
-    __tablename__ = "Image"
-
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(50))
-    title = db.Column(db.String(20))
-    description = db.Column(db.String(100))
-
-    __table_args__ = (UniqueConstraint('filename'),)
-
